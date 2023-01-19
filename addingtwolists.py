4e72dd3d557cb74e09da581cee4521c02e8d5f1b
chaitanya pavankumar <116827919+chaitu3354M@users.noreply.github.com>
2023-01-19 15:28:27 +0530

    Create addingtwolists.py

---
+++ addingtwolists.py
@@ -0,0 +1,13 @@
+l1 = [1,2,3,4,5,8,9,5,4,6]
+l2 = [5,8,6,9,46,4,5]
+addition = [l1+l1]
+print(addition)
+
+
+for i in l1:
+  if i%2==0:
+    print("good day")
+  else:
+    print("see you")
+    
+    
